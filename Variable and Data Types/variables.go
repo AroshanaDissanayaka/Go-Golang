c906ae7921c064fdd5820ec607eea1bdec0396d5
Aroshana Dissanayake <aroshanaprabhathqualifications@gmail.com>
2024-12-02 21:00:38 +0530

    Type Coversions
    How to Use Type Conversions in GO/Golang

--- Variable and Data Types/variables.go
+++ Variable and Data Types/variables.go
@@ -79,4 +79,21 @@ area = pi * radius * radius
 fmt.Println(area);
 
 
+
+//Type Coversions
+
+
+var intNum  int = 10;
+var floatNum float64 = 3.5;
+
+sum := intNum + int(floatNum)
+
+fmt.Println(sum);
+
+sum2 := float64(intNum) + floatNum
+
+fmt.Println(sum2);
+
+
+
 }
